c4b1e0aa06453e38447c0ad6b7e58a394d978968
Muhie Aldeen <zezo1998@hotmail.com>
2024-01-13 14:47:43 +0100

    created navbare

---
+++ next-project/src/app/(dynamic)/recipes/layout.jsx
@@ -0,0 +1,8 @@
+export default function RecipesLayout({ children }) {
+  return (
+    <div>
+      RecipesLayout
+      {children}
+    </div>
+  );
+}
--- next-project/src/app/(dynamic)/recipes/page.jsx
+++ next-project/src/app/(dynamic)/recipes/page.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 export default function Recipes() {
   return <div>Recipes</div>;
 }
