cdfd6a3dccb443086d5b9d879aca14bedb086aa1
Muhie Aldeen <zezo1998@hotmail.com>
2024-01-15 19:42:29 +0100

    I modifid th e home page

--- next-project/src/app/page.jsx
+++ next-project/src/app/page.jsx
@@ -2,5 +2,24 @@ import Image from 'next/image';
 import styles from './page.module.css';
 
 export default function Home() {
-  return <h1>Home </h1>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.col}>
+        <h1 className={styles.title}>
+          Navigate the art of cooking with an inspired journey through
+          delightful recipes.
+        </h1>
+        <p className={styles.description}>
+          Embark on a flavorful journey with DishDiscovery, your passport to a
+          world of culinary wonders. Explore a curated collection of exquisite
+          dishes and global flavors, designed to inspire both seasoned chefs and
+          aspiring cooks. Uncover the art of cooking with our diverse recipes,
+          cooking tips, and cultural insights, turning every meal into a
+          celebration of international cuisine.
+        </p>
+        <button className={styles.button}>recipes</button>
+      </div>
+      <div className={styles.col}>image</div>
+    </div>
+  );
 }
