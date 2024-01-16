b01ff03b92de794dcdce5113ae64c0e50c467508
Muhie Aldeen <zezo1998@hotmail.com>
2024-01-16 16:14:17 +0100

    I am done with the dark mode

--- next-project/src/components/DarkModeToggle/DarkModeToggle.jsx
+++ next-project/src/components/DarkModeToggle/DarkModeToggle.jsx
@@ -1,9 +1,13 @@
+'use client';
+
+import { useContext } from 'react';
 import styles from './DarkModeToggle.module.css';
+import { ThemeContext } from '@/context/ThemeContext';
 
 export default function DarkModeToggle() {
-  const mode = 'light';
+  const { mode, toggle } = useContext(ThemeContext);
   return (
-    <div className={styles.container}>
+    <div className={styles.container} onClick={toggle}>
       <div className={styles.icon}>🌙</div>
       <div className={styles.icon}>🌚</div>
       <div
