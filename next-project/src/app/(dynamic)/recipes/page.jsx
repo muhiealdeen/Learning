cc0ad84471f5c5be970823ba07b1347b08a35b44
Muhie Aldeen <zezo1998@hotmail.com>
2024-01-20 17:09:43 +0100

    I improving the recipes page

--- next-project/src/app/(dynamic)/recipes/page.jsx
+++ next-project/src/app/(dynamic)/recipes/page.jsx
@@ -25,9 +25,7 @@ export default async function Recipes() {
             <Image
               // key={media.id}
               // src={`/images/icons/${media.name}.png`}
-              src={
-                'https://render.fineartamerica.com/images/rendered/search/print/6.5/8/break/images/artworkimages/medium/1/louisiana-saturday-night-dianne-parks.jpg'
-              }
+              src={recipe.image}
               width={350}
               height={250}
               className={styles.image}
@@ -36,8 +34,12 @@ export default async function Recipes() {
             />
           </div>
           <div className={styles.content}>
-            <h1 className={styles.title}>recipe name:{recipe.name}</h1>
-            <p className={styles.text}>recipe ingredient</p>
+            <h1 className={styles.title}>
+              <span>recipe name: </span> {recipe.name}
+            </h1>
+            <p className={styles.text}>
+              recipe ingredient: {recipe.ingredients}
+            </p>
           </div>
         </Link>
       ))}
