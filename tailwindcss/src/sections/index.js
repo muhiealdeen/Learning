ff24af975472069c91352eaa51d818a1f21fc377
Muhie Aldeen <zezo1998@hotmail.com>
2024-02-04 16:34:21 +0100

    I imported  all sections in once in the app file  and made some correction to make it work

--- tailwindcss/src/sections/index.js
+++ tailwindcss/src/sections/index.js
@@ -6,3 +6,14 @@ import CustomerReviews from './CustomerReviews';
 import SpecialOffer from './SpecialOffer';
 import Subscribe from './Subscribe';
 import SuperQuality from './SuperQuality';
+
+export {
+  Hero,
+  Services,
+  PopularProducts,
+  Footer,
+  CustomerReviews,
+  SpecialOffer,
+  Subscribe,
+  SuperQuality,
+};
