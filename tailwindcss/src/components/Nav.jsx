8903ddd6026b90e64e85e37c53757266bb97c7a4
Muhie Aldeen <zezo1998@hotmail.com>
2024-02-05 23:07:59 +0100

    I created ul and li in  the nav component

--- tailwindcss/src/components/Nav.jsx
+++ tailwindcss/src/components/Nav.jsx
@@ -3,10 +3,27 @@ import { hamburger } from '../assets/icons';
 const Nav = () => {
   return (
     <header className="padding-x py-8 absolute z-10 w-full">
-      <nav>
+      <nav className="flex justify-between items-center max-container">
         <a href="/">
           <img src={headerLogo} alt="logo" width={130} height={29} />
         </a>
+        <ul className="flex-1 flex justify-center items-center gap-16 maxc-lg:hidden">
+          <li className="inline-block mr-6">
+            <a href="/">Home</a>
+          </li>
+          <li className="inline-block mr-6">
+            <a href="/">Products</a>
+          </li>
+          <li className="inline-block mr-6">
+            <a href="/">About</a>
+          </li>
+          <li className="inline-block mr-6">
+            <a href="/">Contact</a>
+          </li>
+          <li className="inline-block">
+            <img src={hamburger} alt="menu" width={20} height={20} />
+          </li>
+        </ul>
       </nav>
     </header>
   );
