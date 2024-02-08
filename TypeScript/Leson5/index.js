3870eb924f3407a5b4ee533e24a0aa6e06a4cee2
Muhie Aldeen <zezo1998@hotmail.com>
2024-02-08 19:05:53 +0100

    I am learning Type Annotations And Any Type with examples

--- TypeScript/Leson5/index.js
+++ TypeScript/Leson5/index.js
@@ -1,2 +1,27 @@
-"use strict";
-//# sourceMappingURL=index.js.map
+/*
+  Type Annotations || Signature
+  -- Indicate The Data Type Of Variables
+  -- Indicate The Data Type Of Functions Input/Output
+  -- Objects, etc.
+
+  - Why We Use It ?
+  - Is It Mandatory ?
+  - What Happen If We Didnt Use It ?
+*/
+var theName = 'Elzero';
+// let theAge: number = 40;
+var hire = true;
+var all = 'Elzero Web School';
+var allVars; // any
+theName = 'Osama';
+all = 100;
+// function add(n1: number, n2: number) {
+//   return n1 + n2;
+// }
+// console.log(add(10, 20));
+// console.log(typeof add(10, 20));
+function add(n1, n2) {
+    return n1 + n2;
+}
+console.log(add(10, '20'));
+console.log(typeof add(10, '20'));
