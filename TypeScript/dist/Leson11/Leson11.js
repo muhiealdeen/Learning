bae3ffac6ee6671d806b267b54688b645f3d160d
Muhie Aldeen <zezo1998@hotmail.com>
2024-02-10 22:32:34 +0100

    I am learning anonymous and arrow function in TypeScript

---
+++ TypeScript/dist/Leson11/Leson11.js
@@ -0,0 +1,8 @@
+"use strict";
+const add = function (num1, num2) {
+    return num1 + num2;
+};
+console.log(add(10, 20));
+const addWithArrow = (num1, num2) => num1 + num2;
+console.log(addWithArrow(10, 20));
+//# sourceMappingURL=Leson11.js.map
