33dc2267b57a6ee44bb45c52f2b1f8732365b2b5
Muhie Aldeen <zezo1998@hotmail.com>
2024-02-12 14:00:36 +0100

    I am learning advanced type alias in TypeScript

--- TypeScript/Leson13/Leson13.ts
+++ TypeScript/Leson13/Leson13.ts
@@ -2,3 +2,16 @@
   Data Types
   - Advanced Type Alias
 */
+
+type Buttons = {
+  up: string;
+  down: string;
+  left: string;
+  right: string;
+};
+function getActions(btns: Buttons) {
+  console.log(`Acti For Button Up Is ${btns.up}`);
+  console.log(`Acti For Button Right Is ${btns.right}`);
+  console.log(`Acti For Button Left Is ${btns.left}`);
+  console.log(`Acti For Button Down Is ${btns.down}`);
+}
