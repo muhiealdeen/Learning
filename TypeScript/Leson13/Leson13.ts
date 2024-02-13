227a45c5ebf588bcb6e5ee4b199517721662cf05
Muhie Aldeen <zezo1998@hotmail.com>
2024-02-13 17:49:59 +0100

    I am learning advanced type alias in TypeScript

--- TypeScript/Leson13/Leson13.ts
+++ TypeScript/Leson13/Leson13.ts
@@ -18,6 +18,13 @@ function getActions(btns: Last) {
   console.log(`Acti For Button Right Is ${btns.right}`);
   console.log(`Acti For Button Left Is ${btns.left}`);
   console.log(`Acti For Button Down Is ${btns.down}`);
+  console.log(`Acti For Button x Is ${btns.x}`);
 }
 
-getActions({ up: 'Jump', down: 'go down', left: 'fo left', right: 'got rigt' });
+getActions({
+  up: 'Jump',
+  down: 'go down',
+  left: 'fo left',
+  right: 'got rigt',
+  x: true,
+});
