9232515aee68ca8bb7bd26607d105d861fa87bd7
Muhie Aldeen <zezo1998@hotmail.com>
2024-02-15 18:50:10 +0100

    I am learning Data Types Void and Never in TypeScript

---
+++ TypeScript/Leson16/Leson16.js
@@ -0,0 +1,17 @@
+'use strict';
+function logging(msg) {
+  console.log(msg, 'msg');
+  return;
+}
+console.log(logging('I anm a Messsage'));
+console.log('Test');
+const fail = (msg) => {
+  throw new Error(msg);
+};
+function alwayslog(name) {
+  while (true) {
+    console.log(name);
+  }
+}
+alwayslog('Ahmed');
+//# sourceMappingURL=Leson16.js.map
