d7949feb156ab1825e04571a0e17bae63a800a45
Muhie Aldeen <zezo1998@hotmail.com>
2024-02-15 18:14:00 +0100

    I am learning Data Types Void and Never in TypeScript

---
+++ TypeScript/Leson16/Leson16.ts
@@ -0,0 +1,17 @@
+/*
+  Data Types
+  - Void
+  --- Function That Will Return Nothing
+  --- Function In JavaScript That Not Return A Value Will Show undefined
+  --- undefined is not void
+  - Never
+  --- Return Type Never Returns
+  --- The Function Doesn't Have A Normal Completion
+  --- It Throws An Error Or Never Finishes Running At All "Infinite Loop"
+*/
+function logging(msg: string) {
+  console.log(msg);
+  return msg;
+}
+
+console.log(logging('I anm a Messsage'));
