37deccd75a43b410e10e040a54ebf8ccf0fe7a6a
Muhie Aldeen <zezo1998@hotmail.com>
2024-02-16 18:10:04 +0100

    I am learning Dta type Enums in TypeScript

---
+++ TypeScript/Leson17/Leson17.ts
@@ -0,0 +1,16 @@
+/*
+  Data Types
+  - Enums => Enumerations
+  --- Allow Us To Declare A Set Of Named Constants
+  --- Used For Logical Grouping Collection Of Constants "Collection Of Related Values"
+  --- It Organize Your Code
+  --- By Default Enums Are Number-Based, First Element Is 0
+  --- Theres A Numeric Enums
+  --- Theres A String-Based Enums
+  --- Theres Heterogeneous Enums [String + Number]
+*/
+
+const KIDS = 15;
+const EASY = 9;
+const MEDIUM = 6;
+const HARD = 3;
