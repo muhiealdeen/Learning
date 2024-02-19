d1f1d5884b5fa8a26e47fb399377713b97bcda04
Muhie Aldeen <zezo1998@hotmail.com>
2024-02-19 21:01:24 +0100

    I am learning Union Type in typeScript

---
+++ TypeScript/Leson20/Leson20.ts
@@ -0,0 +1,14 @@
+/*
+  Data Types
+  - Union And Intersection Types
+  --- Union Type
+  ------ The | Symbol Is Used To Create The Union => "Or"
+
+  --- Intersection Type
+  ------ Is A Type That Combines Several Types Into One
+  ------ The & Symbol Is Used To Create An Intersection => "And"
+
+  --- If A Union Is An OR, Then An Intersection Is An AND.
+*/
+
+let number: number | string;
