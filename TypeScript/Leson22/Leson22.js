8b4fc7ca7c2dccb719886d80628ff112a03462ec
Muhie Aldeen <zezo1998@hotmail.com>
2024-02-21 16:42:00 +0100

    I am learning Interface Declaration in typeScript

--- TypeScript/Leson22/Leson22.js
+++ TypeScript/Leson22/Leson22.js
@@ -1,8 +1,14 @@
 "use strict";
 let user = {
-    id: 1,
     username: 'Ahmed',
     country: 'Egypt',
 };
+user.country = 'Syria';
 console.log(user);
+function getData(data) {
+    console.log(`User ID: ${data.id}`);
+    console.log(`User Name: ${data.username}`);
+    console.log(`User Country: ${data.country}`);
+}
+getData({ id: 200, username: 'Ali', country: 'KSA' });
 //# sourceMappingURL=Leson22.js.map
--- TypeScript/Leson22/Leson22.ts
+++ TypeScript/Leson22/Leson22.ts
@@ -11,14 +11,23 @@
 */
 
 interface User {
-  id: number;
-  username: string;
+  id?: number;
+  readonly username: string;
   country: string;
 }
 
 let user: User = {
-  id: 1,
+  // id: 1,
   username: 'Ahmed',
   country: 'Egypt',
 };
+
+user.country = 'Syria';
 console.log(user);
+
+function getData(data: User) {
+  console.log(`User ID: ${data.id}`);
+  console.log(`User Name: ${data.username}`);
+  console.log(`User Country: ${data.country}`);
+}
+getData({ id: 200, username: 'Ali', country: 'KSA' });
