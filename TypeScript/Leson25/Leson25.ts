b3cc2c360f1589836450055a47d20c269ce6f7ce
Muhie Aldeen <zezo1998@hotmail.com>
2024-02-22 14:26:52 +0100

    I am learning Extending Interfaces in typeScript

--- TypeScript/Leson25/Leson25.ts
+++ TypeScript/Leson25/Leson25.ts
@@ -2,3 +2,24 @@
   Interface
   - Extending Interfaces
 */
+interface User2 {
+  id?: number;
+  username: string;
+  country: string;
+}
+interface Moderator {
+  role: string | number;
+}
+interface Admin extends User2, Moderator {
+  protect?: boolean;
+}
+
+let user2: Admin = {
+  id: 100,
+  username: 'Ahmed',
+  country: 'Egypt',
+  role: 'Mod',
+  protect: true,
+};
+
+console.log(user2.id);
