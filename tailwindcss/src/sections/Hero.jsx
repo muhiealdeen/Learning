5e214f6470f33cb123e52ef87a4937a8a79dada2
Muhie Aldeen <zezo1998@hotmail.com>
2024-02-23 22:45:48 +0100

    I am working on hero section

--- tailwindcss/src/sections/Hero.jsx
+++ tailwindcss/src/sections/Hero.jsx
@@ -2,8 +2,10 @@ const Hero = () => {
   return (
     <section
       id="home"
-      className="flex flex-col justify-center w-full min-h-screen gap-10 p-2 border-2 border-red-500 max-container xl:flex-row "
-    ></section>
+      className="flex flex-col justify-center w-full min-h-screen gap-10 max-container xl:flex-row "
+    >
+      <div className="relative flex flex-col items-start justify-center w-full xl:w-2/5 max-xl:padding-x pt-28"></div>
+    </section>
   );
 };
 
