5db51a049c64cb33d618f7c54767be1467151753
Muhie Aldeen <zezo1998@hotmail.com>
2024-02-23 23:57:37 +0100

    I created a button file in the components section and added it to  hero section

--- tailwindcss/src/components/Button.jsx
+++ tailwindcss/src/components/Button.jsx
@@ -1,5 +1,14 @@
-const Button = () => {
-  return <button className="">click me </button>;
+const Button = ({ lable, iconURL }) => {
+  return (
+    <button className="">
+      {lable}
+      <img
+        src={iconURL}
+        alt="arrow right icon"
+        className="w-5 h-5 ml-2 rounded-full"
+      />
+    </button>
+  );
 };
 
 export default Button;
