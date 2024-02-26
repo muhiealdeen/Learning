6412b2e6300c5904c29568fefa9ee0198bb3a882
Muhie Aldeen <zezo1998@hotmail.com>
2024-02-26 21:30:37 +0100

    I am learning Annotation with class in typescript

--- TypeScript/Leson27/Leson27.ts
+++ TypeScript/Leson27/Leson27.ts
@@ -2,16 +2,19 @@
   Type Annotations With Class
 */
 class User {
- 
+  u: string;
+  s: number;
   msg: () => string;
   constructor(username: string, salary: number) {
     this.u = username;
     this.s = salary;
     this.msg = function () {
       return `Hello ${this.u} Your Salary Is ${this.s}`;
-    }
+    };
   }
   sayMsg() {
-   return`Hello ${this.u} Your Salary Is ${this.s}`;
+    return `Hello ${this.u} Your Salary Is ${this.s}`;
   }
-  let userOne =new User ('Elzero', 6000)
+}
+
+let userOne = new User('Elzero', 6000);
