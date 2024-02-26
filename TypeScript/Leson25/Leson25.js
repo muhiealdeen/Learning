f7bb5b061960bb125d45201be86e8cab7b887b11
Muhie Aldeen <zezo1998@hotmail.com>
2024-02-26 21:38:21 +0100

    I am learning Annotation with class in typescript

---
+++ TypeScript/Leson25/Leson25.js
@@ -0,0 +1,10 @@
+"use strict";
+let user2 = {
+    id: 100,
+    username: 'Ahmed',
+    country: 'Egypt',
+    role: 'Mod',
+    protect: true,
+};
+console.log(user2.id);
+//# sourceMappingURL=Leson25.js.map
