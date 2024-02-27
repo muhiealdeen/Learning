8b1b34e6e58907e12d480d86205af4570b3d407b
Muhie Aldeen <zezo1998@hotmail.com>
2024-02-27 22:04:51 +0100

    I am learning Get And Set Accessors in typescript

--- TypeScript/Leson28/leson28.js
+++ TypeScript/Leson28/leson28.js
@@ -1,18 +1,2 @@
 "use strict";
-class User1 {
-    constructor(username, salary, address) {
-        this.username = username;
-        this.salary = salary;
-        this.address = address;
-        this.msg = function () {
-            return `Hello ${this.username} Your Salary Is ${this.salary}`;
-        };
-    }
-    sayMsg() {
-        return `Hello ${this.username} Your Salary Is ${this.salary}`;
-    }
-}
-let userOne1 = new User1(`Elzero`, 6000, `Egypt`);
-console.log(userOne1.msg());
-console.log(userOne1.sayMsg());
 //# sourceMappingURL=leson28.js.map
