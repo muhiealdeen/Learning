35be28ba9430cf48ccc64a2e28ca05a90cde4082
Muhie Aldeen <zezo1998@hotmail.com>
2024-03-01 17:40:48 +0100

    I am developing Populaeproducts folder addig some stile and dive to show the products inside it

--- tailwindcss/src/sections/PopularProducts.jsx
+++ tailwindcss/src/sections/PopularProducts.jsx
@@ -1,18 +1,19 @@
-import { products } from '../constents/index';
+// import { products } from '../constents/index';
 const PopularProducts = () => {
   return (
     <section id="products" className=" max-container max-sm:mt-12">
       <div className=" flex flex-col justify-start gap-5">
         <h2 className=" text-4xl font-palanquin font-bold">
-          Our <span>Populer</span> Products
+          Our <span className=" text-coral-red">Populer</span> Products
         </h2>
-        <p>
+        <p className="lg:max-w-lg mt-2 font-montserrat text-slate-gray">
           Explore our selection of popular products, curated to meet your needs
           and elevate your lifestyle. From stylish apparel to cutting-edge
           gadgets, we have something for everyone. Dive in and discover your
           next favorite item today
         </p>
       </div>
+      <div className=" mt-16 grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2  grid-cols-1"></div>
     </section>
   );
 };
