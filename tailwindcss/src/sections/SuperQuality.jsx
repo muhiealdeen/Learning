7bfe07c6234bd4df4245baa38c16e0ae78313f51
Muhie Aldeen <zezo1998@hotmail.com>
2024-03-02 19:39:30 +0100

    I started developing the superQuality section

--- tailwindcss/src/sections/SuperQuality.jsx
+++ tailwindcss/src/sections/SuperQuality.jsx
@@ -1,5 +1,12 @@
 const SuperQuality = () => {
-  return <div>SuperQuality</div>;
+  return (
+    <section
+      id="about-us"
+      className=" flex justify-between items-center max-lg:flex-col gap-10 w-full max-container "
+    >
+      SuperQuality
+    </section>
+  );
 };
 
 export default SuperQuality;
