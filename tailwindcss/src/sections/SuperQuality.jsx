5c387a443a061b4485f18b405a56e36924f5f623
Muhie Aldeen <zezo1998@hotmail.com>
2024-03-03 13:31:00 +0100

    I am still developing the superQuality section

--- tailwindcss/src/sections/SuperQuality.jsx
+++ tailwindcss/src/sections/SuperQuality.jsx
@@ -21,8 +21,14 @@ const SuperQuality = () => {
           footwear is designed to elevate your experience, providing you with
           unmatched quality,innovation and touch of Elegance.
         </p>
+        <p className=" mt-6 lg:max-w-lg info-text">
+          Our dedication to detail and excellence ensures your satisfaction
+        </p>
 
-        <Button label="View details" />
+        <div className="mt-11">
+          {' '}
+          <Button label="View details" />
+        </div>
       </div>
       SuperQuality
     </section>
