bf3f11db09f6b645ff1d14490dfd1884038d0920
Muhie Aldeen <zezo1998@hotmail.com>
2024-03-03 13:37:54 +0100

    I finishd developing the superQuality section

--- tailwindcss/src/sections/SuperQuality.jsx
+++ tailwindcss/src/sections/SuperQuality.jsx
@@ -1,3 +1,4 @@
+import { shoe8 } from '../assets/images';
 import Button from '../components/button';
 
 const SuperQuality = () => {
@@ -26,11 +27,18 @@ const SuperQuality = () => {
         </p>
 
         <div className="mt-11">
-          {' '}
           <Button label="View details" />
         </div>
       </div>
-      SuperQuality
+      <div className=" flex-1 flex justify-center items-center ">
+        <img
+          src={shoe8}
+          alt="shoe8"
+          width={570}
+          height={522}
+          className=" object-contain"
+        />
+      </div>
     </section>
   );
 };
