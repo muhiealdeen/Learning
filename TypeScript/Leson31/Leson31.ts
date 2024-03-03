fa2e2f31d1b36dff3c8e353c7ac7e4659d9b6f46
Muhie Aldeen <zezo1998@hotmail.com>
2024-03-03 20:45:32 +0100

    I learning class implement interface in typeScript

--- TypeScript/Leson31/Leson31.ts
+++ TypeScript/Leson31/Leson31.ts
@@ -2,3 +2,21 @@
   Class
   - Implement Interface
 */
+interface Settings5 {
+  theme: boolean;
+  font: string;
+  sane(): void;
+}
+class User5 implements Settings5 {
+  constructor(public username: string, public theme: boolean) {}
+  font: string;
+  sane(): void {
+    throw new Error('Method not implemented.');
+  }
+  save(): void {
+    console.log(`Saved`);
+  }
+  update(): void {
+    console.log(`Updated`);
+  }
+}
