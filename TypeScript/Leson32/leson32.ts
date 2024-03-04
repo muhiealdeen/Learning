f889eb06357752afe0dafe65b2910716c8bd958b
Muhie Aldeen <zezo1998@hotmail.com>
2024-03-04 14:10:38 +0100

    I learning Abstract Classes And Members in typeScript

--- TypeScript/Leson32/leson32.ts
+++ TypeScript/Leson32/leson32.ts
@@ -4,14 +4,23 @@
   --- We Cannot Create An Instance Of An Abstract Class
 */
 
-class Food {
-  getCookingTime(): void {
-    console.log(' Coooking Time Is ...');
-  }
-}
-class Pizza extends Food {
-  // getCookingTime (): void {
-  //   console.log(" Coooking Time Is 20 Minutes");
+abstract class Food {
+  constructor(public title: string) {}
+  abstract getCookingTime(): void;
+  // {
+  //   console.log(' Coooking Time Is ...');
   // }
 }
-class Burger extends Food {}
+class Pizza extends Food {
+  constructor(title: string, public price: number) {
+    super(title);
+  }
+  getCookingTime(): void {
+    console.log(' Coooking Time For Pizzaq Is 60 Minutes');
+  }
+}
+class Burger extends Food {
+  getCookingTime(): void {
+    console.log(' Coooking Time For Burger Is 30 Minutes');
+  }
+}
