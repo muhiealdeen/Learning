cf2401f5e0d63899649a189a003d06f724ee7b80
Muhie Aldeen <zezo1998@hotmail.com>
2024-03-05 16:09:46 +0100

    I learning class Polymorphism & Method Override in typeScript

---
+++ TypeScript/leson33/Leson33.ts
@@ -0,0 +1,20 @@
+/*
+  Class
+  - Polymorphism & Method Override
+
+  - Polymorphism
+  --- Classes Have The Same Methods But Different Implementations
+
+  - Method Override
+  --- Allowing Child Class To Provide Implementation Of A Method In Parent Class
+  --- A Method In Child Class Must Have Same Name As Parent Class
+
+  --- noImplicitOverride
+*/
+class Player { 
+  constructor (public name: string) {
+    attack(): void {
+      console.log(`Attacking Now`);
+    }
+  }
+  } 
