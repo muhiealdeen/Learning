6f3c0cb0f0a8470258ab70be3f10ddbce5508c33
Muhie Aldeen <zezo1998@hotmail.com>
2024-03-12 16:02:02 +0100

    I learning  Generics Multiple Types in typeScript

--- TypeScript/Leson35/Leson35.ts
+++ TypeScript/Leson35/Leson35.ts
@@ -4,3 +4,12 @@
   - Multiple Types
   - Discussion
 */
+function returnType<T>(val: T): T {
+  return val;
+}
+console.log(returnType<number>(10));
+console.log(returnType<string>('Hello'));
+
+const returntypeArrowSyntax = <T>(val: T): T => val;
+console.log(returntypeArrowSyntax<number>(10));
+console.log(returntypeArrowSyntax<string>('Hello'));
