f1276dd9c6d3b63146e923ab8f9890ada05b6a8e
Muhie Aldeen <zezo1998@hotmail.com>
2024-03-12 16:09:25 +0100

    I learning  Generics Multiple Types in typeScript

--- TypeScript/Leson35/Leson35.js
+++ TypeScript/Leson35/Leson35.js
@@ -7,4 +7,9 @@ console.log(returnType('Hello'));
 const returntypeArrowSyntax = (val) => val;
 console.log(returntypeArrowSyntax(10));
 console.log(returntypeArrowSyntax('Hello'));
+function textType(val) {
+    return `The Value is ${val} The Type Is ${typeof val}`;
+}
+console.log(textType(10));
+console.log(textType('Hello'));
 //# sourceMappingURL=Leson35.js.map
