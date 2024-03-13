97d8e95017c786d8830ac17d0a01dc8b12ab871a
Muhie Aldeen <zezo1998@hotmail.com>
2024-03-13 16:40:08 +0100

    I am developing the CustomerReviews section

--- tailwindcss/src/sections/CustomerReviews.jsx
+++ tailwindcss/src/sections/CustomerReviews.jsx
@@ -1,5 +1,16 @@
 const CustomerReviews = () => {
-  return <div>CustomerReviews</div>;
+  return (
+    <section className=" max-container">
+      <h3 className=" font-palanquin text-center text-4xl font-bold ">
+        What Our
+        <span className="text-coral-red"> Customer</span> Say?
+      </h3>
+      <p>
+        Here genuine stories from our satisfied customers about their
+        exceptional experience with us.
+      </p>
+    </section>
+  );
 };
 
 export default CustomerReviews;
