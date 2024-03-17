241f11129d5627e7ff5af787959e64eafd14b11b
Muhie Aldeen <zezo1998@hotmail.com>
2024-03-17 16:37:12 +0100

    I done the  footer section

--- tailwindcss/src/sections/Footer.jsx
+++ tailwindcss/src/sections/Footer.jsx
@@ -1,4 +1,7 @@
+// import { Link } from 'react-router-dom';
 import { footerLogo } from '../assets/images';
+import { footerLinks, socialMedia } from '../constants';
+import { copyrightSign } from '../assets/icons';
 
 const Footer = () => {
   return (
@@ -12,10 +15,50 @@ const Footer = () => {
             Get shoes ready for the new term at your nearest Nike store. Find
             your perfect Size In store. Get Rewards
           </p>
-          <div className=""></div>
+          <div className=" flex items-center gap-5 mt-8">
+            {socialMedia.map((icon) => (
+              <div
+                className="flex justify-center items-center w-12 h-12 bg-white rounded-full"
+                key={icon.alt}
+              >
+                <img src={icon.src} alt={icon.alt} width={24} height={24} />
+              </div>
+            ))}
+          </div>
+        </div>
+        <div className="flex flex-1 justify-between lg:gap-10 gap-20 flex-wrap">
+          {footerLinks.map((section) => (
+            <div key={section}>
+              <h4 className="text-white font-montserrat leading-normal font-medium mb-6">
+                {section.title}
+              </h4>
+              <ul>
+                {section.links.map((link) => (
+                  <li
+                    className="mt-3 font-montserrat text-base leading-normal text-white-400 hover:text-slate-gray cursor-pointer"
+                    key={link.name}
+                  >
+                    <a href={link.link}>{link.name}</a>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          ))}
         </div>
       </div>
-      Footer
+      <div className=" flex justify-between text-white-400 mt-24 max-sm:flex-col max-sm:items-center">
+        <div className=" flex flex-1 justify-start items-center gap-2 font-montserrat cursor-pointer">
+          <img
+            src={copyrightSign}
+            alt="copy right sign "
+            width={20}
+            height={20}
+            className=" rounded-full m-0"
+          />
+          <p>Copyright. All rights reserved.</p>
+        </div>
+        <p className="font-montserrat cursor-pointer">Terms & Conditions</p>
+      </div>
     </footer>
   );
 };
