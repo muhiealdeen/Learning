f24bde338fb4d0d865ba72c6ff1bba85f2b67044
Muhie Aldeen <zezo1998@hotmail.com>
2024-03-17 14:02:11 +0100

    I am improving the reviewcardsubsicrib section

--- tailwindcss/src/sections/Subscribe.jsx
+++ tailwindcss/src/sections/Subscribe.jsx
@@ -1,7 +1,10 @@
 const Subscribe = () => {
   return (
     <section className=" max-container flex justfy-between items-center max-lg:flex-col gap-10">
-      Subscribe
+      <h3 className=" text-4xl leading-[68px] lg:max-w-md font-palanquin font-bold">
+        Sign Up Form
+        <span className="text-coral-red"> Updates</span> & Newsletter
+      </h3>
     </section>
   );
 };
