8941ce2f6292dbee6b8a6d66316e71193cc9ea58
Muhie Aldeen <zezo1998@hotmail.com>
2024-03-17 14:45:35 +0100

    I am developing footer section

--- tailwindcss/src/sections/Footer.jsx
+++ tailwindcss/src/sections/Footer.jsx
@@ -1,7 +1,15 @@
+import { footerLogo } from '../assets/images';
+
 const Footer = () => {
   return (
     <footer className="max-container">
-      <div></div>
+      <div className=" flex justify-between items-start gap-20 flex-wrap max-lg:flex-col ">
+        <div className=" flex flex-col items-start">
+          <a href="/">
+            <img src={footerLogo} alt="" />
+          </a>
+        </div>
+      </div>
       Footer
     </footer>
   );
