dbf5af6da5b9be3dc415eb408ca4af9da1f9829e
Muhie Aldeen <zezo1998@hotmail.com>
2024-03-21 14:39:17 +0100

    I am learning Generics Classes And Interfaces in TypeScript

--- TypeScript/Leson37/Leson37.js
+++ TypeScript/Leson37/Leson37.js
@@ -7,6 +7,10 @@ class Collection {
         this.data.push(itme);
     }
 }
+let itemOne = new Collection();
+itemOne.add({ itemType: 'Book', title: 'Atomic Habits', isbn: 12345 });
+itemOne.add({ itemType: 'Book', title: 'Follow Your Heart', isbn: 54321 });
+console.log(itemOne);
 let itemTwo = new Collection();
 itemTwo.add({
     itemType: 'Book',
