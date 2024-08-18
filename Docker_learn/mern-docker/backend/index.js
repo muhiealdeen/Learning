65e9fec36a73194a3a042ab869534df573eb0c8f
Muhie Aldeen <zezo1998@hotmail.com>
2024-08-18 18:22:09 +0200

    I am still learnin how to create dockerr imge and container for Nextjs app

--- Docker_learn/mern-docker/backend/index.js
+++ Docker_learn/mern-docker/backend/index.js
@@ -1,43 +1,43 @@
-const express = require("express");
-const cors = require("cors");
-const dotenv = require("dotenv");
-const colors = require("colors");
+const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const colors = require('colors');
 
 dotenv.config();
 colors.enable();
 
-const Anim = require("./database/anim.model");
-const connect = require("./database/connect");
+const Anim = require('./database/anim.model');
+const connect = require('./database/connect');
 
 const app = express();
 
 app.use(
   cors({
-    origin: "*",
-  })
+    origin: '*',
+  }),
 );
 
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  console.log("Hello World!".rainbow);
+app.get('/', (req, res) => {
+  console.log('Hello World!'.rainbow);
 
-  res.send("Hello World!");
+  res.send('Hello World!');
 });
 
-app.get("/api/anime", async (req, res) => {
+app.get('/api/anime', async (req, res) => {
   const anime = await Anim.find();
   res.json(anime);
 });
 
-app.post("/api/anime", async (req, res) => {
+app.post('/api/anime', async (req, res) => {
   const anime = new Anim(req.body);
   await anime.save();
   res.json(anime);
 });
 
 app.listen(8000, () => {
-  console.log("server listening on port 8000");
+  console.log('server listening on port 8000');
 
   // connect to the database
   connect();
